ebaf319b6f077863b87a387f248cee61d8f3ef4c
Junsu <somadubel@gmail.com>
2022-03-14 03:07:11 +0900

    ADD : 창 생성

---
+++ Learn OpenGL/Hello Window.cpp
@@ -0,0 +1,53 @@
+#include <glad/glad.h> // glad 헤더파일을 먼저 포함시켜야 한다.
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+	glfwInit(); // GLFW 초기화
+
+	// glfwWindowHint함수를 통해 GLFW 설정
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // 버전 x.x에서 앞자리 3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // 버전 x.x에서 뒷자리 3 
+	// => 즉, OpenGL 버전 3.3을 사용한다고 명시함.
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // core-profile을 사용한다고 명시
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// GLAD 초기화
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
